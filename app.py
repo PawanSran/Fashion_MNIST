7d52b2d4201ccec79041240f9c50ff8ab2ec2c8f
Pawanjeet Kaur <pawanjeetkaur@Pawanjeets-MacBook-Pro.local>
2020-10-08 15:53:20 -0500

    adding port information to app.py

--- app.py
+++ app.py
@@ -94,4 +94,5 @@ def predict_1():
 
 if __name__ == "__main__":
     # start the flask app, allow remote connections
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port = port)
